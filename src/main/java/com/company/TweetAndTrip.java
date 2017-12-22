4516b038ea17def1496fe26c96e04626d11f82cd
jose <jorollu@hotmail.com>
2017-12-22 19:51:14 +0100

    Main API modified to get 5 destinations instead of 1

--- src/main/java/com/company/TweetAndTrip.java
+++ src/main/java/com/company/TweetAndTrip.java
@@ -9,14 +9,20 @@ import twitter4j.TwitterException;
 import twitter4j.User;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayesMultinomialText;
+import weka.core.Instance;
 import weka.core.Instances;
+import weka.filters.unsupervised.instance.RemovePercentage;
 
 import static spark.Spark.*;
 
 public class TweetAndTrip {
 
+    private static final int MAX_DESTINATIONS = 5;
     private static MysqlConnect mysqlConnect = new MysqlConnect();
 
     public static void main(String[] args) {
@@ -30,10 +36,10 @@ public class TweetAndTrip {
             String name = req.queryParams("name");
             String jsonInString;
             try {
-                Destination destination = recommendDestination(name);
+                ArrayList<Destination> destinations = recommendDestination(name);
                 Gson gson = new Gson();
 
-                jsonInString = gson.toJson(destination);
+                jsonInString = gson.toJson(destinations);
             } catch (TwitterException e){
                 e.printStackTrace();
                 System.out.println("Failed to get timeline: " + e.getMessage());
@@ -49,7 +55,77 @@ public class TweetAndTrip {
         });
     }
 
-    private static Destination recommendDestination(String username) throws Exception {
+    private static void evaluateTopN(String fullData, int N) throws Exception {
+        boolean doCV=false;
+        BufferedReader in = new BufferedReader(new FileReader(fullData));
+        Instances data = new Instances(in);
+        in.close();
+
+        data.setClassIndex(data.numAttributes()-1);
+
+        if (doCV){
+            Classifier cls = new NaiveBayesMultinomialText();
+            Evaluation eval = new Evaluation(data);
+            eval.crossValidateModel(cls, data, 5, new Random(1L));
+        }
+
+        RemovePercentage filter = new RemovePercentage();
+        filter.setPercentage(0.8);
+        filter.setInputFormat(data);
+        Instances test=filter.getOutputFormat();
+        filter.setInvertSelection(true);
+        Instances train=filter.getOutputFormat();
+
+        Classifier cls = new NaiveBayesMultinomialText();
+        cls.buildClassifier(train);
+
+        int tp=0;
+        int total=0;
+
+        for (Instance i: test){
+            total++;
+            double[] dist = cls.distributionForInstance(i);
+            int c = i.classIndex();
+            //List<Integer> dests = getSortedDestinationIds(dist);
+            List<Integer> ids = new ArrayList<>();
+            // calculate top N
+            Map<Double, Set<Integer>> map = new HashMap<>();
+            for (int j=0; j<dist.length; j++){
+                Set<Integer> idx = map.get(dist[j]);
+                if (idx==null){
+                    idx = new HashSet<>();
+                    map.put(dist[j], idx);
+                }
+                idx.add(j);
+            }
+            List<Double> probs = new ArrayList<>(map.keySet());
+            Collections.sort(probs, Collections.reverseOrder());
+            int n = 0;
+/*            for (Double d: probs){
+                for (Integer idx: map.get(d)){
+                    if (n >= N){
+                        break;
+                    }
+                    if(idx == c){
+                        // match: success
+                        tp++;
+                        break;
+                    }
+                    n++;
+                }
+            }*/
+            for (Double d: probs){
+                for (Integer idx: map.get(d)){
+                    if (n >= N){
+                        break;
+                    }
+                    ids.add(idx);
+                }
+            }
+        }
+        System.out.println("True positive at " + N + ": " + (1.0*tp/total));
+    }
+    private static ArrayList<Destination> recommendDestination(String username) throws Exception {
         /**1. Get user name
          * 2. Get location, language, gender, description, tags, ?
          * 3. Build new .arff with those fields
@@ -68,7 +144,29 @@ public class TweetAndTrip {
         /**Instances from new file**/
         Instances instances = WekaUtils.getWekaInstance();
         double value = cls.classifyInstance(instances.get(0));
-        //get the name of the class value
+        //Sort all classes by probability
+
+        //1. Get all probabilities into a hashmap
+        Map<Integer, Double> classesProbability = new HashMap<>();
+        double[] distributionForInstance = cls.distributionForInstance(instances.get(0));
+        for(int i = 0; i < distributionForInstance.length; i++){
+              classesProbability.put(i, distributionForInstance[i]);
+        }
+        //2. Sort hashmap
+        //JAVA 8 SORTING
+        //TODO REFACTOR INTO ARRAYLIST
+        Map<Integer, Double> result = classesProbability.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+        //get the name of the classes value
+        //3. Get classes
+        ArrayList<String> destinations = new ArrayList<>();
+
+        for(int i = 0; i < MAX_DESTINATIONS; i++){
+            destinations.add(instances.classAttribute().value(new ArrayList<>(result.keySet()).get(i)));
+        }
         String prediction = instances.classAttribute().value((int)value);
 
         System.out.println("The predicted value of instance " +
@@ -77,27 +175,31 @@ public class TweetAndTrip {
 
         /**FOURSQUARE**/
         FoursquareUtils foursquareUtils = new FoursquareUtils();
-        ArrayList<Venue> userVenues = new ArrayList<>();
         /**With user tag and recommended destination**/
         Iterator<String> iterator = tweetAndTripUser.getHashtags().iterator();
+        ArrayList<Destination> finalDestinations = new ArrayList<>();
         /**Limiting foursquare calls to avoid ban**/
-        while(iterator.hasNext() && userVenues.size() < 2) {
-            ArrayList<CompactVenue> foursquareVenues = foursquareUtils.searchVenues(prediction, iterator.next());
-            for (CompactVenue venue : foursquareVenues) {
-                userVenues.add(new Venue(venue.getName(),
-                        venue.getCategories().length > 0 ? venue.getCategories()[0].getName() : null,
-                        venue.getLocation().getCity()));
+        for (String dest : destinations) {
+            ArrayList<Venue> userVenues = new ArrayList<>();
+            while (iterator.hasNext() && userVenues.size() < 2) {
+                ArrayList<CompactVenue> foursquareVenues = foursquareUtils.searchVenues(dest, iterator.next());
+                for (CompactVenue venue : foursquareVenues) {
+                    userVenues.add(new Venue(venue.getName(),
+                            venue.getCategories().length > 0 ? venue.getCategories()[0].getName() : null,
+                            venue.getLocation().getCity()));
+                }
             }
+            finalDestinations.add(new Destination(dest, userVenues));
         }
-
         /***RECOMMENDED DESTINATION BASED ON YOUR INFO:***/
         System.out.println("FINAL RECOMENDATION: ");
         System.out.println("For user: " + tweetAndTripUser.getUsername());
         System.out.println("Destination: " + prediction);
-        for (Venue venue : userVenues) {
+        /*for (Venue venue : userVenues) {
             System.out.println("Venues: " + venue.name);
-        }
-        return new Destination(prediction, userVenues);
+        }*/
+        //TODO GET TRUE POSITIVES TO GET REAL CLASSIFICATION
+        return finalDestinations;
     }
 
     private static void fillData() {
